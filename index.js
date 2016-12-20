7b3dd519c912cd8689bb88d318fc193674eb8674
Denise Lim <denise.lim.sh@gmail.com>
2016-12-20 11:05:47 +0800

    program works

--- index.js
+++ index.js
@@ -1,5 +1,86 @@
-function intro() {
-  return "Hello World!"
+function Hotel(name, rating, weekdayRegular, weekdayRewards, weekendRegular, weekendRewards) {
+  this.name = name;
+  this.rating = rating;
+  this.weekdayRegular = weekdayRegular;
+  this.weekdayRewards = weekdayRewards;
+  this.weekendRegular = weekendRegular;
+  this.weekendRewards = weekendRewards;
+  this.price = 0;
+
 }
 
-module.exports = {intro: intro}
+var lakewood = new Hotel("Lakewood", 3, 110, 80, 90, 80)
+var bridgewood = new Hotel ("Bridgewood", 4, 160, 110, 60, 50)
+var ridgewood = new Hotel ("Ridgewood", 5, 220, 110, 150, 40)
+
+var hotels = [lakewood, bridgewood, ridgewood]
+
+function checkCheapest(details) {
+  details = parseData(details)
+  var days = checkDays(details.dates)
+
+  for (var i = 0; i< hotels.length; i++) {
+    if (details.user == "Regular") {
+      hotels[i].price = (hotels[i].weekdayRegular * days.weekdays) + (hotels[i].weekendRegular * days.weekends)
+    } else {
+      hotels[i].price = (hotels[i].weekdayRewards * days.weekdays) + (hotels[i].weekendRewards * days.weekends)
+    }
+  }
+
+  var cheapest = lakewood
+
+  for (var i = 0; i < hotels.length; i++) {
+    if (hotels[i].price < cheapest.price) {
+      cheapest = hotels[i]
+    } else if (hotels[i].price == cheapest.price && hotels[i].rating > cheapest.rating) {
+      cheapest = hotels[i]
+    }
+  }
+
+  return hotel[i].name
+}
+
+function checkDays(data) {
+  var daysTracker = {
+    weekdays: 0,
+    weekends: 0
+  }
+
+  for (var i = 0; i < data.length; i++) {
+    var givenDate = new Date (data[i]).getDay()
+    if (givenDate == 0 || givenDate == 6) {
+      daysTracker.weekends++
+    } else {
+      daysTracker.weekdays++
+    }
+  }
+  return daysTracker
+}
+
+function parseData(booking) {
+  var parseBooking = {
+    user: "",
+    dates: []
+  }
+
+  parseBooking.user = booking.split(": ")[0]
+  var tempDates = booking.split(": ")[1]
+  tempDates = tempDates.split(", ")
+
+  for (var i = 0; i < tempDates.length; i++) {
+    tempDates[i] = tempDates[i].split("(")[0].split("")
+    tempDates[i].splice(2, 0, " ")
+    tempDates[i].splice(tempDates[i].length - 4, 0, " ")
+    tempDates[i] = tempDates[i].join("")
+    parseBooking.dates.push(tempDates[i])
+  }
+  return parseBooking
+}
+
+// ======================================================================
+
+module.exports = {
+  lakewood: lakewood,
+  parseData: parseData,
+  checkDays: checkDays
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,70 @@
 var assert = require('chai').assert
 var test = require('../index')
 
-describe("Files link up", function() {
-  it("should return Hello World", function() {
-    assert.equal(test.intro(), "Hello World!")
+describe("Hotel Constructor - Creating hotel objects", function() {
+  it("should return an object for Lakewood", function() {
+    assert.isObject(test.lakewood)
+  })
+
+  it("should return rating of 3 for Lakewood", function() {
+    assert.equal(test.lakewood.rating, 3)
   })
 })
+
+describe("parseData - refining customer input", function() {
+  it("should return an object", function() {
+    assert.isObject(test.parseData("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
+  })
+
+  it("should return Regular", function() {
+    assert.equal(test.parseData("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)").user, "Regular")
+  })
+
+  it("should return array length of 3", function() {
+    assert.equal(test.parseData("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)").dates.length, 3)
+  })
+
+  it("should return date of 17 Mar 2009", function() {
+    assert.equal(test.parseData("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)").dates[1], "17 Mar 2009")
+  })
+})
+
+describe("checkDays - updating weekdays & weekends", function() {
+  it("should return an object", function() {
+    assert.isObject(test.checkDays(["16 Mar 2009", "17 Mar 2009", "18 Mar 2009"]))
+  })
+
+  it("should return 3 weekdays", function() {
+    assert.equal(test.checkDays(["16 Mar 2009", "17 Mar 2009", "18 Mar 2009"]).weekdays, 3)
+  })
+
+  it("should return 0 weekend", function() {
+    assert.equal(test.checkDays(["16 Mar 2009", "17 Mar 2009", "18 Mar 2009"]).weekends, 0)
+  })
+
+  it("should return 1 weekdays", function() {
+    assert.equal(test.checkDays(["20 Mar 2009", "21 Mar 2009", "22 Mar 2009"]).weekdays, 1)
+  })
+
+  it("should return 2 weekends", function() {
+    assert.equal(test.checkDays(["20 Mar 2009", "21 Mar 2009", "22 Mar 2009"]).weekends, 2)
+  })
+})
+
+describe("checkCheapest - check cheapest hotel", function() {
+  it("should return Lakewood", function() {
+    assert.isObject(test.checkDays("Regular: 17Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)", "Lakewood"))
+  })
+
+  it("should return Bridgewood", function() {
+    assert.isObject(test.checkDays("Regular: 20Mar2009(fri). 21Mar2009(sat), 22Mar2009(sun)", "Bridgewood"))
+  })
+
+  it("should return Ridgewood", function() {
+    assert.isObject(test.checkDays("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)", "Ridgewood"))
+  })
+})
+
+// "Regular: 17Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)" => Lakewood
+// "Regular: 20Mar2009(fri). 21Mar2009(sat), 22Mar2009(sun)" => Bridgewood
+// "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)" => Ridgewood
